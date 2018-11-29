231347aca2e664b6692979da310661f80d03cf30
Vlad Mescherkin <vmescherkin@gmail.com>
2018-11-29 17:57:01 +0100

    chapter 7 done

--- palindrome.rb
+++ palindrome.rb
@@ -1,5 +1,22 @@
-# Returns true for a palindrome, false otherwise.
-def palindrome?(string)
-  processed_content = string.downcase
-  processed_content == processed_content.reverse
+module Palindrome
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      self.to_s.downcase
+    end
+end
+
+class String
+  include Palindrome
+end
+
+class Integer
+  include Palindrome
 end
